b440d374a48bd6bf95b66ca9dddae7b6793b37b6
gichangseo <skc1395@naver.com>
2018-02-15 22:37:39 +0900

    Modified templates to display posts from database.

--- app/views.py
+++ app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Room, RoomType, RoomAgree, Option
 
 def room_list(request):
-    return render(request, 'app/room_list.html', {})
+    rooms = Room.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'app/room_list.html', {'rooms':rooms})
