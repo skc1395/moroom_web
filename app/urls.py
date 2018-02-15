3c3f7814d7781083788914fa98942c78109dcfa7
gichangseo <skc1395@naver.com>
2018-02-16 00:32:00 +0900

    Extended to detail.html

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.room_list, name='room_list'),
+    url(r'^room/(?P<pk>\d+)/$', views.room_detail, name='room_detail'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Room, RoomType, RoomAgree, Option
 
 def room_list(request):
     rooms = Room.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'app/room_list.html', {'rooms':rooms})
+
+def room_detail(request, pk):
+    room = get_object_or_404(Room, pk=pk)
+    return render(request, 'app/room_detail.html', {'room': room})
