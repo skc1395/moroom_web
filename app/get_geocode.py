549833d0643af552df3f98675c4035fdfb0ecbe2
gichangseo <skc1395@naver.com>
2018-02-27 22:37:35 +0900

    13

--- app/get_geocode.py
+++ app/get_geocode.py
@@ -1,7 +1,7 @@
 import requests
 
 def Get_geocode(address,key):
-    url = 'https://maps.googleapis.com/maps/api/geocode/json?address={}&key={}'.format(address,key)
+    url = 'https://maps.googleapis.com/maps/api/geocode/json?address={}&components=country:KR&key={}'.format(address,key)
     req = requests.get(url)
     response = req.json()
     lng = response['results'][0]['geometry']['location']['lng']
