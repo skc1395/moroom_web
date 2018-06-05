f9c46653b7b4bde3a9ccfaef1fd8fb1b3a470b51
gichangseo <skc1395@naver.com>
2018-06-05 23:50:30 +0900

    6/5(2)

--- app/get_geocode.py
+++ app/get_geocode.py
@@ -1,30 +1,28 @@
 import requests
 
-
 def Get_geocode(address, key):
- """
- 사용자가 주소 입력시, DB에 저장하기 전 view단에서 damm map api를 이용해서
- 주소값을 올바른 경도, 위도 값으로 변환후, db에 저장해준다.
+    """
+    사용자가 주소 입력시, DB에 저장하기 전 view단에서 damm map api를 이용해서
+    주소값을 올바른 경도, 위도 값으로 변환후, db에 저장해준다.
 
- 만약 주소값에 오류가 있다면 -> api 요청 결과, 빈 리스트이면(IndexError)
- lng와 lat 값으로 0으로 주고, 사용자가 입력한 문(gate)의 위도와 경도값을 저장한다.
+    만약 주소값에 오류가 있다면 -> api 요청 결과, 빈 리스트이면(IndexError)
+    lng와 lat 값으로 0으로 주고, 사용자가 입력한 문(gate)의 위도와 경도값을 저장한다.
 
- """
-     url = 'https://dapi.kakao.com/v2/local/search/address.json'
-     headers = {'Authorization': 'KakaoAK {}'.format(key)}
-     data = {'query': address}
-     req = requests.get(url, headers=headers, data=data)
+    """
+
+    url = 'https://dapi.kakao.com/v2/local/search/address.json'
+    headers = {'Authorization': 'KakaoAK {}'.format(key)}
+    data = {'query': address}
+    req = requests.get(url, headers=headers, data=data)
 
 
-     response = req.json()
+    response = req.json()
     lng = response['documents'][0]['address']['y']
     lat = response['documents'][0]['address']['x']
 
     return (lng,lat)
 
 
-
-    
     try:
         lng = response['documents'][0]['address']['y']
         lat = response['documents'][0]['address']['x']
@@ -34,4 +32,4 @@ def Get_geocode(address, key):
         lng = 0
         lat = 0
 
-    return (lng,lat)
+        return (lng,lat)
