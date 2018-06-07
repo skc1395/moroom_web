f3cf1b5ab6caa0cdfe89f71160f858535b0d6e49
saz0549 <saz0549@naver.com>
2018-06-07 14:41:07 +0900

    데이터베이스 학교, 문 속성 수정(주소, 좌표 추가)

--- app/models.py
+++ app/models.py
@@ -29,16 +29,35 @@ class Option(models.Model):
         return self.name
 
 class University(models.Model):
-    name_eng = models.CharField(max_length=50)
+    """
+    name_eng : 대학교 영어명(url로 이용됨) -> 학교 약자가 아님 전체이름으로 사용 ex. 경북대학교 -> kyungpook
+    name_kor : 대학교 이름
+    address : 중앙 좌표에 대한 주소값을 저장한다.
+    location : 학교의 중앙 좌표값을 저장한다.
+    """
+    name_eng = models.CharField(max_length=50) 
     name_kor = models.CharField(max_length=50)
+    address = models.CharField(max_length=100, default="")
+    location_long = models.CharField(max_length=50)
+    location_lat = models.CharField(max_length=50)
     
 
     def __str__(self):
         return self.name_kor
 
 class Door(models.Model):
+    """
+    name : 문 이름(학교마다 동일한 이름이 존재할 수 있음) 경북대학교 - 동문
+    address : 문에 대한 주소를 검색
+    location : 문 부근의 좌표 저장
+    * 사용자가 방 정보를 입력시에 주소값이 올바르지 않으면 잘못된 좌표값 요청
+      따라서, 잘못된 값을 가지고 있을시(0,0) 문의 좌표값으로 방의 위치를 대체함.
+    """
     name = models.CharField(max_length=30)
-    university = models.ManyToManyField(University)
+    university = models.ForeignKey(University, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100, default="")
+    location_long = models.CharField(max_length=50)
+    location_lat = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
@@ -51,13 +70,11 @@ class Room(models.Model):
     published_date = models.DateTimeField(blank=True, null=True)
     deposit = models.IntegerField()
     rentfee = models.IntegerField()
-    # deposit_new = models.IntegerField()
-    # rentfee_new = models.IntegerField()
     fee_new = models.CharField(max_length=50)
     manage_fee = models.CharField(max_length=30)
     address = models.CharField(max_length=100)
-    location_long = models.CharField(max_length=50)
-    location_lat = models.CharField(max_length=50)
+    location_long = models.CharField(max_length=50) # 방이 위치한 좌표.
+    location_lat = models.CharField(max_length=50) # 방이 위치한 좌표.
     date_start = models.DateField()
     date_end = models.DateField()
     room_type = models.ForeignKey(RoomType)
